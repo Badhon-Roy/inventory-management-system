bf14b41495f2d8f4621deefc44cde005ef34bdf6
Badhon-Roy <roybadhon286@gmail.com>
2023-11-26 04:47:02 +0600

    check out product want to incress sale count , decrease quantity and convert ot jsPDF

--- src/Pages/Dashboard/CheckOut/CheckOut.jsx
+++ src/Pages/Dashboard/CheckOut/CheckOut.jsx
@@ -1,15 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
-import { useParams } from "react-router-dom";
 import useAxiosSecure from "../../../Hook/useAxiosSecure";
+import { jsPDF } from "jspdf";
 
 
 const CheckOut = () => {
     const axiosSecure = useAxiosSecure()
-    const { id } = useParams()
-    const { data, isLoading } = useQuery({
+    const { data, isLoading, refetch } = useQuery({
         queryKey: ['products'],
         queryFn: async () => {
-            const res = await axiosSecure.get(`/products/${id}`)
+            const res = await axiosSecure.get('/checkOut')
             return res.data
         }
     })
@@ -20,40 +19,140 @@ const CheckOut = () => {
     }
     const currentDate = new Date()
     const formateDate = currentDate.toLocaleString()
-    const handleSales = ()=>{
-        const salesInfo = {
-            product_name : data?.product_name,
-            sales_date : formateDate
+    const handleSales = (id) => {
+        const selectedProduct = data?.find((product) => product._id === id);
+        const doc = new jsPDF();
+        doc.setFontSize(12);
+        const topMargin = 10;
+        const leftMargin = 10;
+        const pageSize = {
+            width: 210,
+            height: 297
+        };
+        const availableWidth = pageSize.width - leftMargin * 2;
+        doc.text(`
+    Shop Name         : ${selectedProduct?.shop_name}
+    Product Name      : ${selectedProduct?.product_name}
+    Product ID        : ${selectedProduct?.product_id}
+    Product Location  : ${selectedProduct?.product_location}
+    Description       : ${selectedProduct?.description}
+    Product Discount  : ${selectedProduct?.discount}"%"
+    Selling Price     : ${selectedProduct?.selling_price}
+    ----------------
+    Manager Name      : ${selectedProduct?.name}
+    Manager Email     : ${selectedProduct?.email}
+    Product Date      : ${selectedProduct?.date}
+    Check Out Date    : ${formateDate}
+    
+`, 10, 10 + 60);
+        if (selectedProduct?.product_image) {
+            doc.addImage(selectedProduct?.product_image, 'JPEG', 50, 10, 50, 50);
         }
-        axiosSecure.post('/sales' , salesInfo)
-        .then(res =>{
-            console.log(res.data);
-        })
+        doc.setLineWidth(0.5);
+        doc.line(leftMargin, topMargin + 200, leftMargin + availableWidth, topMargin + 200);
+
+        doc.save("a4.pdf");
+
+
+
+
+
+
+
+        const salesInfo = {
+            product_name: selectedProduct?.product_name,
+            sales_date: formateDate
+        }
+        axiosSecure.post('/sales', salesInfo)
+            .then(res => {
+                console.log(res.data);
+            })
         const incrementInfo = {
-            sale_count : data.sale_count + 1
+            sale_count: selectedProduct?.sale_count + 1
         }
         const decrementInfo = {
-            quantity : data.quantity - 1
+            quantity: selectedProduct?.quantity - 1
         }
-        axiosSecure.put(`/products/${id}/increment` , incrementInfo)
-        .then(res =>{
-            console.log(res.data);
-        })
-        axiosSecure.put(`/products/${id}/decrement` , decrementInfo)
-        .then(res =>{
-            console.log(res.data);
-        })
+        axiosSecure.put(`/products/${selectedProduct?.product_id}/increment`, incrementInfo)
+            .then(res => {
+                console.log(res.data);
+            })
+        axiosSecure.put(`/products/${selectedProduct?.product_id}/decrement`, decrementInfo)
+            .then(res => {
+                console.log(res.data);
+            })
+
+
+        axiosSecure.delete(`/checkOut/${selectedProduct?._id}`)
+            .then(res => {
+                console.log('delete', res.data);
+                if (res.data.deletedCount > 0) {
+                    refetch();
+                }
+            })
     }
     return (
         <div>
-            <h2>THis is check out {id} {data?.product_name} </h2>
-            <div className="border flex items-center">
-                <div>
-                    <img className="w-[200px]" src={data?.product_image} alt="" />
-                </div>
-                <h2>{data?.product_name}</h2>
-            </div>
-            <button onClick={handleSales} className="BTN mt-3">Get Paid</button>
+            {
+                data?.length > 0 ? <div>
+                    <h2 className="md:text-4xl text-2xl text-center mb-8 underline">Total {data?.length} products is Check Out </h2>
+                    <div className="overflow-x-auto">
+                        <table className="table">
+                            {/* head */}
+                            <thead>
+                                <tr>
+                                    <th>
+                                        <label>
+                                            #
+                                        </label>
+                                    </th>
+                                    <th>Name</th>
+                                    <th>Quantity</th>
+                                    <th>Cost</th>
+                                    <th>Sale Count</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    data?.map((item , index) =>
+                                        <tr key={item?._id}>
+                                            <th>
+                                                <label>
+                                                    {index + 1}
+                                                </label>
+                                            </th>
+                                            <td>
+                                                <div className="flex items-center gap-3">
+                                                    <div className="avatar">
+                                                        <div className="rounded-md w-12 h-12">
+                                                            <img src={item?.product_image} />
+                                                        </div>
+                                                    </div>
+                                                    <div>
+                                                        <div className="font-bold">{item?.product_name}</div>
+                                                        <div className="text-sm opacity-50">location: {item?.product_location}</div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                {item?.quantity}
+                                            </td>
+                                            <td>{item?.cost}</td>
+                                            <td>{item?.sale_count}</td>
+                                            <th>
+                                                <button onClick={() => handleSales(item?._id)} className="BTN">Get Paid</button>
+                                            </th>
+                                        </tr>
+
+
+                                    )
+                                }
+                            </tbody>
+
+                        </table>
+                    </div>
+                </div> : <p className="text-4xl font-bold text-center my-16">You are not check out any product</p>
+            }
         </div>
     );
 };
